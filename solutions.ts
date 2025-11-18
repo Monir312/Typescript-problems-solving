57168457a15e7b49aaf1243ff5ea7210dfe93db6
Monir Islam <monirislam8426@gmail.com>
2025-11-18 16:31:17 +0800

    updated

--- solutions.ts
+++ solutions.ts
@@ -98,19 +98,30 @@ function getUniqueValues(arr1: (string | number)[], arr2: (string | number)[]): 
   const result: (string | number)[] = []
 
   for (let i = 0; i < arr1.length; i++) {
-    if (!result.includes(arr1[i])) {
-      result.push(arr1[i])
+    let exists = false
+    for (let j = 0; j < result.length; j++) {
+      if (arr1[i] === result[j]) {
+        exists = true
+        break
+      }
     }
+    if (!exists) result.push(arr1[i])
   }
 
   for (let i = 0; i < arr2.length; i++) {
-    if (!result.includes(arr2[i])) {
-      result.push(arr2[i])
+    let exists = false
+    for (let j = 0; j < result.length; j++) {
+      if (arr2[i] === result[j]) {
+        exists = true
+        break
+      }
     }
+    if (!exists) result.push(arr2[i])
   }
 
   return result
 }
+
 
 
 
